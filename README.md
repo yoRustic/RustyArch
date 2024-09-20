d2189526e5ab946dfc67641ed137034a1085e545
yoRustic <jaydansabourin07@gmail.com>
2024-09-19 23:21:00 -0400

    v1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # RustyArch
+simple install script for a fresh Arch linux with yay
+
+install packages by hand with this commnad
+```
+yay -S hyprland-bin kitty waybar-hyprland \
+    swaybg rofi mako thunar \
+    ttf-jetbrains-mono-nerd noto-fonts-emoji \
+    polkit-gnome python-requests zsh \
+    swappy grim slurp pamixer brightnessctl gvfs \
+    bluez bluez-utils lxappearance xfce4-settings \
+    xdg-desktop-portal-hyprland-git chrome fastfetch vesktop-bin
+```
---
+++ RustyArch.sh
@@ -0,0 +1,59 @@
+#### Check for yay ####
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "yay was located, moving on.\n"
+    yay -Suy
+else 
+    echo -e "yay was not located, please install yay. Exiting script.\n"
+    exit 
+fi
+
+### Disable wifi powersave mode ###
+read -n1 -rep 'Would you like to disable wifi powersave? (y,n)' WIFI
+if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
+    LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
+    echo -e "The following has been added to $LOC.\n"
+    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
+    echo -e "\n"
+    echo -e "Restarting NetworkManager service...\n"
+    sudo systemctl restart NetworkManager
+    sleep 3
+fi
+
+### Install all of the packages ####
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    yay -S --noconfirm hyprland-bin kitty waybar-hyprland \
+    swaybg rofi mako thunar \
+    ttf-jetbrains-mono-nerd noto-fonts-emoji \
+    polkit-gnome python-requests zsh \
+    swappy grim slurp pamixer brightnessctl gvfs \
+    bluez bluez-utils lxappearance xfce4-settings \
+    xdg-desktop-portal-hyprland-git chrome fastfetch vesktop-bin
+
+    # Start the bluetooth service
+    echo -e "Starting the Bluetooth Service...\n"
+    sudo systemctl enable --now bluetooth.service
+    sleep 2
+    
+    # Clean out other portals
+    echo -e "Cleaning out conflicting xdg portals...\n"
+    yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+fi
+
+### Install the ohmyzsh shell ###
+read -n1 -rep 'Would you like to install the ohmyzsh shell? (y,n)' STAR
+if [[ $STAR == "Y" || $STAR == "y" ]]; then
+    # install the ohmyzsh shell
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+### Script is done ###
+echo -e "Script had completed.\n"
+echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep 'Would you like to start Hyprland now? (y,n)' HYP
+if [[ $HYP == "Y" || $HYP == "y" ]]; then
+    exec Hyprland
+else
+    exit
+fi
